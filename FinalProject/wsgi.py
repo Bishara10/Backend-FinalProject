090e7470e2f8bf448883f4d0b1ee1a714d8c8984
Bishara1 <pyder5@hotmail.com>
2024-07-20 17:10:56 +0300

    configurations for render

--- FinalProject/wsgi.py
+++ FinalProject/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FinalProject.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FinalProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'render_deploy.settings')
 
 application = get_wsgi_application()
 
